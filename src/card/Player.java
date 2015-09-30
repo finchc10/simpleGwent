b1a3d623b82b97b5e3f0178db3c2b693b0a08302
Cory <cory.m.finch@gmail.com>
2015-09-29 20:03:11 -0400

    created player class

---
+++ src/card/Player.java
@@ -0,0 +1,32 @@
+package card;
+
+public class Player 
+{
+	private String name;
+	private int    score = 0;
+	
+	public Player(String name)
+	{
+		this.name = name;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public int getScore()
+	{
+		return score;
+	}
+	
+	public String toString()
+	{
+		return getName() + ": " + score;
+	}
+	
+	public void incrementScore(int increase)
+	{
+		score = score + increase;
+	}
+}
